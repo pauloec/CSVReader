eb2cf87508c8411530df80b8d4b6be8af570b53a
Paulo <paulo.ec@hotmail.com>
2022-04-13 15:29:33 +0200

    Remove storyboard and add Coordinator

--- CSVReader/SceneDelegate.swift
+++ CSVReader/SceneDelegate.swift
@@ -6,17 +6,30 @@
 //
 
 import UIKit
+import RootCoordinator
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var rootCoordinator: RootCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        UINavigationBar.appearance().customNavigationBar()
+
+        let navigationController = UINavigationController()
+        rootCoordinator = RootCoordinator(navigationController: navigationController)
+        rootCoordinator?.start()
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.overrideUserInterfaceStyle = .dark
+        window?.rootViewController = navigationController
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
